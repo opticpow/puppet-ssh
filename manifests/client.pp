844c6064c446b4c5fc5ce7db1c7cf837723d4606
Wayne Ingram <w.ingram@nib.com.au>
2014-01-09 15:09:49 +1100

    Suse Tweaks

--- manifests/client.pp
+++ manifests/client.pp
@@ -1,8 +1,6 @@
 class ssh::client {
   include ssh::params
-  if $osfamily != 'Suse' {
-    include ssh::client::install
-  }
+  include ssh::client::install
   include ssh::client::config
   include ssh::knownhosts
 }
