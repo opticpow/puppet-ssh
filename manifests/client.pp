6296984b3cfbbfcca534dc58745dfe76b7e55f79
Wayne Ingram <w.ingram@nib.com.au>
2014-01-09 15:04:37 +1100

    Initial Suse Support

--- manifests/client.pp
+++ manifests/client.pp
@@ -1,6 +1,8 @@
 class ssh::client {
   include ssh::params
-  include ssh::client::install
+  if $osfamily != 'Suse' {
+    include ssh::client::install
+  }
   include ssh::client::config
   include ssh::knownhosts
 }
